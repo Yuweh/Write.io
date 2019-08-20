7229591eaa8beed2473a8c9e3046e8e1ff9b4ff7
Jay <Yuweh@users.noreply.github.com>
2019-08-20 14:42:50 +0800

    ¯\_(ツ)_/¯  for improvement

--- TodayExt/TodayVC.swift
+++ TodayExt/TodayVC.swift
@@ -36,16 +36,32 @@
     // set key for saving
     NSString* glucoseTimeStampKey = [localTimeString stringByAppendingString:@"-InputGlucoseTimeStampKey"];
     NSString* glucoseDateStampKey = [localDateString stringByAppendingString:@"-InputGlucoseDateStampKey"];
+    NSString* glucoseTimeStampArrayKey = [localDateString stringByAppendingString:@"-InputGlucoseDateStampArrayKey"];
     
     // for checking
     NSLog(@"localDateString        :%@", localDateString);
     NSLog(@"localTimeString       :%@", localTimeString);
     NSLog(@"utcDate       :%@", dateToday);
-    NSLog(@"keyGlucoseArray       :%@", glucoseTimeStampKey);
+    NSLog(@"glucoseTimeStampKey       :%@", glucoseTimeStampKey);
     
     //save inputglucose
     NSUserDefaults *shared = [[NSUserDefaults alloc] initWithSuiteName:@"group.jp.co.arkray.e-SMBG"];
-    [shared setObject:glucoseTimeStampKey forKey:glucoseDateStampKey];
+    //[shared setObject:glucoseTimeStampKey forKey:glucoseDateStampKey];
+    
+    // initialize empty arrays and dictionary
+    NSMutableArray *timeStampsArray = [[NSMutableArray alloc]init];
+    NSMutableArray *tempArray = [[NSMutableArray alloc]init];
+    NSMutableDictionary *tempDic = [[NSMutableDictionary alloc]init];
+    
+    // save new user input with time stamp in dictionary
+    [tempDic setObject:userGlucoseInput forKey:glucoseTimeStampKey];
+    // save used time stamp in a seperate array
+    [timeStampsArray addObject:glucoseTimeStampKey];
+    // save new dictionary input to array of input recorded for today
+    [tempArray addObject:tempDic];
+    
+    // save values for Data Sharing with extension
+    [shared setObject:tempArray forKey:glucoseDateStampKey];
+    [shared setObject:timeStampsArray forKey:glucoseTimeStampArrayKey];
     [shared synchronize];
 }
-
